33f5831423cd84346175f215eb6ada1da7e51920
OlehBrov <o.dudnyk85@gmail.com>
2023-03-26 13:17:17 +0300

    onclick hook fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,32 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { ModalWindow } from './Modal.Styled';
 
 export const Modal = props => {
-    const onModalClick = (e) => {
-    console.log("🚀 ~ file: Modal.jsx:16 ~ onModalClick ~ e:", e)
-    
-    e.preventDefault();
-    if (e.code === 'Escape' || e.type === 'click')
-      props.modalWindowHandler({ pageURL: null });
-  };
+  const onModalClick = useCallback(
+    e => {
+      e.preventDefault();
+      if (e.code === 'Escape' || e.type === 'click')
+        props.modalWindowHandler({ pageURL: null });
+    },
+    [props]
+  );
   useEffect(() => {
     window.addEventListener('click', onModalClick);
     window.addEventListener('keydown', onModalClick);
     document.body.style.overflow = 'hidden';
-    console.log('first useEffect')
     return () => {
       window.removeEventListener('click', onModalClick);
       window.removeEventListener('keydown', onModalClick);
       document.body.style.overflow = 'unset';
-      console.log('return useEffect')
     };
   }, [onModalClick]);
 
   return (
-    <ModalWindow className="overlay" onClick={()=> onModalClick()}>
+    <ModalWindow className="overlay" onClick={() => onModalClick()}>
       <div className="modal">{props.children}</div>
     </ModalWindow>
   );
 };
-
-
